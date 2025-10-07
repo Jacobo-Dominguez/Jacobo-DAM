d51293476feb6891ebf4e7db2897459212daa1da
Jacobo Luis Dominguez Morales <jacobo9810@gmail.com>
2025-10-07 17:47:58 +0200

    Proyecto C#
    Extensión del proyecto Dinero hecho en Python por el compañero , pero esta vez en C#

---
+++ INTERFACES/ClaseDineroExtra/Ingreso.cs
@@ -0,0 +1,13 @@
+﻿public class Ingreso : Dinero
+{
+    public DateTime Fecha { get; set; }
+
+    public Ingreso(double cantidad, string descripcion, DateTime fecha) : base(cantidad, descripcion)
+    {
+        Fecha = fecha;
+    }
+    public override string ToString()
+    {
+        return $"Ingreso: {Cantidad}, Descripcion: {Descripcion} ({Fecha})";
+    }
+}
