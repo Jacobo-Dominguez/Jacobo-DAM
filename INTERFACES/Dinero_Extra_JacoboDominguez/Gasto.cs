21570a83163ed79da6cc24089546f061c80603f4
Jacobo Luis Dominguez Morales <jacobo9810@gmail.com>
2025-10-07 17:53:16 +0200

    Proyecto C#
    Extensión del proyecto Dinero hecho en Python por el compañero , pero esta vez en C#

---
+++ INTERFACES/Dinero_Extra_JacoboDominguez/Gasto.cs
@@ -0,0 +1,10 @@
+﻿public class Gasto : Dinero
+{
+    public Gasto(double cantidad, string descripcion) : base(cantidad, descripcion)
+    {
+    }
+    public override string ToString()
+    {
+        return $"Gasto: {Descripcion}, Cantidad: {Cantidad}";
+    }
+}
