7e3fcf6b9a3a3512a3c4bfeb7b4c076df60f6805
Jacobo-Dominguez <jacobo9810@gmail.com>
2025-11-17 22:35:20 +0100

    Clases DAO e interfaces referentes a las clases entidades

---
+++ Acceso a datos/Refugio_Hibernate/src/main/java/org/example/dao/AnimalDAO.java
@@ -0,0 +1,49 @@
+package org.example.dao;
+
+import org.example.entities.Animal;
+import java.util.List;
+
+/**
+ * DAO (Data Access Object)
+ *
+ * Create / Read / Update / Delete
+ */
+public interface AnimalDAO {
+
+    /**
+     * @return todos los animales
+     */
+    List<Animal> findAll();
+
+    /**
+     * @param id
+     * @return devuelve un animal por un id concreto
+     */
+    Animal findById(Integer id);
+
+    /**
+     * @param especie
+     * @return devuelve más de un animal por especie
+     */
+    List<Animal> findByEspecie(String especie);
+
+    // Inserta un nuevo registro
+    Animal create(Animal animal);
+
+    // Actualizar
+    Animal update(Animal animal);
+
+    /**
+     * @param id
+     * @return borra un id concreto
+     */
+    boolean deleteById(Integer id);
+
+    /**
+     * Actualiza solo el estado de un animal concreto
+     * @param id id del animal
+     * @param nuevoEstado nuevo valor para el campo estado
+     * @return true si se ha actualizado, false si no existe el animal
+     */
+    boolean updateEstado(Integer id, String nuevoEstado);
+}
