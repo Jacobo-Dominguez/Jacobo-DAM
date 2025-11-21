a81868976972e8734cda1113a1d3a7ca011454f6
Jacobo-Dominguez <jacobo9810@gmail.com>
2025-11-21 17:40:39 +0100

    Añadido perfil a la barra nav superior

--- Optativa (PHP)/BlogPersonal/views/layout/header.php
+++ Optativa (PHP)/BlogPersonal/views/layout/header.php
@@ -9,10 +9,15 @@
 <body>
 <header class="site-header">
     <div class="container">
-        <div class="brand">BlogPersonal</div>
+        <div class="brand"><a href="?route=home" style="color:inherit; text-decoration:none;">BlogPersonal</a></div>
         <nav class="nav">
             <?php if (!empty($_SESSION['user'])): ?>
-                <span class="who">Conectado: <?php echo htmlspecialchars($_SESSION['user']['name']); ?></span>
+                <a class="profile-link" href="?route=profile" title="Ver perfil">
+                    <?php if (!empty($_SESSION['user']['avatar_url'])): ?>
+                        <img src="<?= htmlspecialchars($_SESSION['user']['avatar_url']) ?>" alt="avatar" style="width:28px;height:28px;border-radius:50%;vertical-align:middle;margin-right:6px;">
+                    <?php endif; ?>
+                    <span class="who"><?= htmlspecialchars($_SESSION['user']['name']) ?></span>
+                </a>
                 <a class="btn" href="?route=post/create">Crear post</a>
                 <a class="btn" href="?route=logout">Cerrar sesión</a>
             <?php else: ?>
