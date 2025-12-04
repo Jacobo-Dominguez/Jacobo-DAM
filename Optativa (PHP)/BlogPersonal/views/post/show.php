8dda5c870f309f59a7f197b43eefc1548c6d3280
Jacobo-Dominguez <jacobo9810@gmail.com>
2025-12-04 18:52:40 +0100

    Cuando pulsas Volver se mantiene en la pagina de Moderar posts

--- Optativa (PHP)/BlogPersonal/views/post/show.php
+++ Optativa (PHP)/BlogPersonal/views/post/show.php
@@ -5,5 +5,13 @@
         <img class="full-image" src="/public/uploads/<?php echo htmlspecialchars($post['image']); ?>" alt="">
     <?php endif; ?>
     <div class="content"><?php echo nl2br(htmlspecialchars($post['content'])); ?></div>
-    <p><a class="btn" href="?route=home">Volver</a></p>
+    <p>
+        <?php 
+        $backUrl = '?route=home';
+        if (isset($_GET['from']) && $_GET['from'] === 'moderate') {
+            $backUrl = '?route=post/moderate';
+        }
+        ?>
+        <a class="btn" href="<?= $backUrl ?>">Volver</a>
+    </p>
 </section>
